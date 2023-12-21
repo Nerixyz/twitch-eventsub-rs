83a7ea20849f75a37bc4742bbb503b19bd7e9609
Nerixyz <nerixdev@outlook.de>
2023-12-21 20:04:37 +0100

    test: add test for guards

--- actix-web-eventsub/tests/util.rs
+++ actix-web-eventsub/tests/util.rs
@@ -1,7 +1,30 @@
 use tokio::process::Command;
 
-pub const SECRET: &'static [u8] =
-    b"5f5f121fc807a21bab4209b2f34e90932778f12c099ca3ca17ee00afd0b328ba";
+pub const SECRET: &[u8] = b"5f5f121fc807a21bab4209b2f34e90932778f12c099ca3ca17ee00afd0b328ba";
+// Second secret used to separate different routes
+pub const SECRET2: &[u8] = b"5f5f121fc807a21bab4209b2f34e90932778f12c099ca3ca17ee00afd0b328bb";
+
+pub trait SecretConfig {
+    fn secret() -> &'static [u8];
+}
+
+macro_rules! make_secret_configs {
+    ($($name:ident => $evaluate:expr,)*) => {
+        $(
+            pub struct $name;
+            impl SecretConfig for $name {
+                fn secret() -> &'static [u8] {
+                    $evaluate
+                }
+            }
+        )*
+    };
+}
+
+make_secret_configs!(
+    BaseSecret => SECRET,
+    SecondSecret => SECRET2,
+);
 
 pub async fn twitch_cli(args: impl FnOnce(&mut Command)) {
     let mut cmd = Command::new("twitch");
